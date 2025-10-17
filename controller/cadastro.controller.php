448106a240157c5bd90997de6c2ff9277a3171bb
Tamar <tamarpeixoto1@gmail.com>
2025-10-17 13:16:08 -0300

    parte-8

--- controller/cadastro.controller.php
+++ controller/cadastro.controller.php
@@ -3,22 +3,8 @@ $cadastro = $_POST['Nome'];
 $sobrenome = $_POST['Sobrenome'];
 $email = $_POST['email'];
 $senha = $_POST['Senha'];
-$senha_confirmation = $_POST['Senha_confirmation'];
-    
-if ($senha === $senha_confirmation) {
-    // Cadastrar o usuário
+$senha_confirmacao = $_POST['Senha_confirmacao'];
+
+if($senha === $senha_confirmacao){
     include_once '../conexao.php';
-    $sql = "INSERT INTO usuarios (Nome, Sobrenome, email, Senha) VALUES ('$cadastro', '$sobrenome', '$email', '$senha')";
-    if (mysqli_query($conn, $sql)) {
-        // Cadastro bem-sucedido
-        echo "Usuário cadastrado com sucesso!";
-        header("Location: ../persistence/cadastro.html?success=1");
-    } else {
-        // Erro ao cadastrar usuário
-        header("Location: ../persistence/inicio.html?error=cadastro_failed");
-    }
-    mysqli_close($conn);
-} else {
-    // Senhas não coincidem
-    header("Location: ../persistence/cadastro.html?error=senha_confirmation");
-}   
+}
--- controller/login.controller.php
+++ controller/login.controller.php
@@ -2,13 +2,3 @@
 $login = $_POST['Nome'];
 $senha = $_POST['Senha'];
 
-//exemplo sem banco de dados teste
-//mudar quando incluir o banco de dados
-if($login == "adm" && $senha == "123456"){
-    session_start();
-    $_SESSION['nome'] = $login;
-    header("Location:\persistence\index.html");
-    exit();
-}else{
-    echo "Usuário ou senha inválidos";
-}
